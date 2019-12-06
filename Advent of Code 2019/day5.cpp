9ce20f1cc44a9d42d3dc66611efcac9154947db4
Sjon Kosse <sjon@x61.eu>
2019-12-06 20:48:17 +0100

    pre merge

--- Advent of Code 2019/day5.cpp
+++ Advent of Code 2019/day5.cpp
@@ -4,6 +4,13 @@
 #include <map>
 #include <optional>
 #include <vector>
+#include <fstream>
+#include <filesystem>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+
+using namespace boost::algorithm;
 
 enum class opCodeID {
 	halt = 99,
@@ -15,6 +22,7 @@ enum class opCodeID {
 	jumpFalse = 6,
 	less = 7,
 	equals = 8,
+	outputScreen = 9,
 };
 
 enum class parameterMode {
@@ -24,77 +32,16 @@ enum class parameterMode {
 
 class intComputer {
 private:
-	std::deque<int> inputBuffer{ 5 };
+	std::deque<int> inputBuffer{ 1 };
 	std::deque<int> output;
-
-	std::vector<int> instructions{
-		3,     225,   1,     225,   6,     6,     1100, 1,     238,  225,   104,
-		0,     1101,  81,    30,    225,   1102,  9,    63,    225,  1001,  92,
-		45,    224,   101,   -83,   224,   224,   4,    224,   102,  8,     223,
-		223,   101,   2,     224,   224,   1,     224,  223,   223,  1102,  41,
-		38,    225,   1002,  165,   73,    224,   101,  -2920, 224,  224,   4,
-		224,   102,   8,     223,   223,   101,   4,    224,   224,  1,     223,
-		224,   223,   1101,  18,    14,    224,   1001, 224,   -32,  224,   4,
-		224,   1002,  223,   8,     223,   101,   3,    224,   224,  1,     224,
-		223,   223,   1101,  67,    38,    225,   1102, 54,    62,   224,   1001,
-		224,   -3348, 224,   4,     224,   1002,  223,  8,     223,  1001,  224,
-		1,     224,   1,     224,   223,   223,   1,    161,   169,  224,   101,
-		-62,   224,   224,   4,     224,   1002,  223,  8,     223,  101,   1,
-		224,   224,   1,     223,   224,   223,   2,    14,    18,   224,   1001,
-		224,   -1890, 224,   4,     224,   1002,  223,  8,     223,  101,   3,
-		224,   224,   1,     223,   224,   223,   1101, 20,    25,   225,   1102,
-		40,    11,    225,   1102,  42,    58,    225,  101,   76,   217,   224,
-		101,   -153,  224,   224,   4,     224,   102,  8,     223,  223,   1001,
-		224,   5,     224,   1,     224,   223,   223,  102,   11,   43,    224,
-		1001,  224,   -451,  224,   4,     224,   1002, 223,   8,    223,   101,
-		6,     224,   224,   1,     223,   224,   223,  1102,  77,   23,    225,
-		4,     223,   99,    0,     0,     0,     677,  0,     0,    0,     0,
-		0,     0,     0,     0,     0,     0,     0,    1105,  0,    99999, 1105,
-		227,   247,   1105,  1,     99999, 1005,  227,  99999, 1005, 0,     256,
-		1105,  1,     99999, 1106,  227,   99999, 1106, 0,     265,  1105,  1,
-		99999, 1006,  0,     99999, 1006,  227,   274,  1105,  1,    99999, 1105,
-		1,     280,   1105,  1,     99999, 1,     225,  225,   225,  1101,  294,
-		0,     0,     105,   1,     0,     1105,  1,    99999, 1106, 0,     300,
-		1105,  1,     99999, 1,     225,   225,   225,  1101,  314,  0,     0,
-		106,   0,     0,     1105,  1,     99999, 8,    226,   677,  224,   1002,
-		223,   2,     223,   1006,  224,   329,   1001, 223,   1,    223,   7,
-		226,   226,   224,   102,   2,     223,   223,  1006,  224,  344,   101,
-		1,     223,   223,   108,   677,   677,   224,  1002,  223,  2,     223,
-		1006,  224,   359,   101,   1,     223,   223,  1107,  226,  677,   224,
-		1002,  223,   2,     223,   1005,  224,   374,  101,   1,    223,   223,
-		1008,  677,   226,   224,   1002,  223,   2,    223,   1005, 224,   389,
-		101,   1,     223,   223,   1007,  677,   226,  224,   1002, 223,   2,
-		223,   1005,  224,   404,   1001,  223,   1,    223,   1107, 677,   226,
-		224,   1002,  223,   2,     223,   1005,  224,  419,   1001, 223,   1,
-		223,   108,   677,   226,   224,   102,   2,    223,   223,  1006,  224,
-		434,   1001,  223,   1,     223,   7,     226,  677,   224,  102,   2,
-		223,   223,   1005,  224,   449,   1001,  223,  1,     223,  107,   226,
-		226,   224,   102,   2,     223,   223,   1006, 224,   464,  101,   1,
-		223,   223,   107,   677,   226,   224,   102,  2,     223,  223,   1006,
-		224,   479,   101,   1,     223,   223,   1007, 677,   677,  224,   1002,
-		223,   2,     223,   1006,  224,   494,   1001, 223,   1,    223,   1008,
-		226,   226,   224,   1002,  223,   2,     223,  1006,  224,  509,   101,
-		1,     223,   223,   7,     677,   226,   224,  1002,  223,  2,     223,
-		1006,  224,   524,   1001,  223,   1,     223,  1007,  226,  226,   224,
-		102,   2,     223,   223,   1006,  224,   539,  101,   1,    223,   223,
-		8,     677,   226,   224,   1002,  223,   2,    223,   1006, 224,   554,
-		101,   1,     223,   223,   1008,  677,   677,  224,   102,  2,     223,
-		223,   1006,  224,   569,   101,   1,     223,  223,   1108, 677,   226,
-		224,   102,   2,     223,   223,   1005,  224,  584,   101,  1,     223,
-		223,   107,   677,   677,   224,   102,   2,    223,   223,  1006,  224,
-		599,   1001,  223,   1,     223,   1108,  677,  677,   224,  1002,  223,
-		2,     223,   1006,  224,   614,   1001,  223,  1,     223,  1107,  677,
-		677,   224,   1002,  223,   2,     223,   1005, 224,   629,  1001,  223,
-		1,     223,   108,   226,   226,   224,   1002, 223,   2,    223,   1005,
-		224,   644,   101,   1,     223,   223,   8,    226,   226,  224,   1002,
-		223,   2,     223,   1005,  224,   659,   101,  1,     223,  223,   1108,
-		226,   677,   224,   1002,  223,   2,     223,  1006,  224,  674,   101,
-		1,     223,   223,   4,     223,   99,    226 };
+	std::vector<int> instructions;
 
 	std::map<opCodeID, size_t> const length{
-		{opCodeID::halt, 0},      {opCodeID::plus, 3},   {opCodeID::multiply, 3},
-		{opCodeID::store, 1},     {opCodeID::output, 1}, {opCodeID::jumpTrue, 2},
-		{opCodeID::jumpFalse, 2}, {opCodeID::less, 3},   {opCodeID::equals, 3},
+		{opCodeID::halt, 0},      {opCodeID::plus, 3},
+		{opCodeID::multiply, 3},  {opCodeID::store, 1},
+		{opCodeID::output, 1},    {opCodeID::jumpTrue, 2},
+		{opCodeID::jumpFalse, 2}, {opCodeID::less, 3},
+		{opCodeID::equals, 3},    {opCodeID::outputScreen, 1},
 	};
 
 	template <typename T>
@@ -120,8 +67,8 @@ private:
 
 	auto write(int val) -> void { output.emplace_back(val); }
 
-	auto apply(opCodeID op, std::vector<parameterMode> mode, std::vector<int> param)
-		-> std::optional<int> {
+	auto apply(opCodeID op, std::vector<parameterMode> mode,
+		std::vector<int> param) -> std::optional<int> {
 		using namespace std::placeholders;
 
 		auto readArguments = [&mode, &param, this](size_t n) -> std::vector<int> {
@@ -139,8 +86,7 @@ private:
 		};
 
 		switch (op) {
-		case opCodeID::halt:
-		{
+		case opCodeID::halt: {
 			throw std::domain_error("We shouldn't have entered here with a halt");
 		}
 		case opCodeID::plus: {
@@ -191,21 +137,17 @@ private:
 			break;
 		}
 
-		case opCodeID::jumpFalse:
-		{
+		case opCodeID::jumpFalse: {
 			auto args = readArguments(length.at(op));
 
-			if (args[0] == 0)
-				return args[1];
+			if (args[0] == 0) return args[1];
 			break;
 		}
 
-		case opCodeID::jumpTrue:
-		{
+		case opCodeID::jumpTrue: {
 			auto args = readArguments(length.at(op));
 
-			if (args[0] != 0)
-				return args[1];
+			if (args[0] != 0) return args[1];
 			break;
 		}
 
@@ -225,35 +167,65 @@ private:
 			write(value);
 			break;
 		}
+		case opCodeID::outputScreen: {
+			int value = 0;
+			if (mode[0] == parameterMode::immediate)
+				value = param[0];
+			else if (mode[0] == parameterMode::position)
+				value = instructions[size_t(param[0])];
+
+			std::cout << (char(char(value)));
+			break;
+		}
 		};
 		return std::nullopt;
 	}
 
-	public:
+public:
 	int compute() {
 		for (auto current = instructions.begin(); current < instructions.end();
-			current++) {
+			) {
 			auto [op, flags] = decodeOpcode(*current);
 			std::vector<int> params(flags.size());
 			for (auto i = 0; i < params.size(); i++) {
 				current++;
 				params[i] = *current;
 			}
-			if (op == opCodeID::halt)
-				break;
+			if (op == opCodeID::halt) break;
 			if (auto offset = apply(op, flags, params); offset != std::nullopt)
-				current = instructions.begin() + size_t(offset.value() - 1);
-
+				current = instructions.begin() + size_t(offset.value());
+			else
+					current++;
 			// for (auto i = instructions.begin(); i < instructions.end(); i++)
 			//  std::cout << *i << '\n';
 		}
-		//for (auto i = output.begin(); i < output.end(); i++) std::cout << *i << '\n';
-		return output.back();
+		// for (auto i = output.begin(); i < output.end(); i++) std::cout << *i <<
+		// '\n';
+		if (output.size() > 0)
+			return output.back();
+		else
+			return 0;
 	}
+	intComputer(std::vector<int> code) :instructions(code) {}
 };
 
 void day5() {
-	intComputer comp;
-	std:: cout << comp.compute() << '\n';
+	auto fileName = "input_ascii_day5.txt";
+	std::ifstream file(fileName);
+	const auto fileSize = std::filesystem::file_size(fileName);
+	auto buf = new char[fileSize];
+	file.read(buf, fileSize);
+
+	auto input = std::string(buf);
+
+	std::vector<std::string> words;
+	split(words, input, is_any_of(","));
+
+	std::vector<int> instructions(words.size());
+	for (size_t i = 0; i < words.size(); i++) {
+		instructions[i] = std::stoi(words[i]);
+	}
+
+	intComputer comp(instructions);
+	std::cout << comp.compute() << '\n';
 }
-	
